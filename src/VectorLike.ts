4f77c3af4ab4ad8a900e99c7f48ae877ea6d87d1
noname <48761044+noname0310@users.noreply.github.com>
2022-09-28 11:51:14 +0900

    create vectorlike interface

---
+++ src/VectorLike.ts
@@ -0,0 +1,17 @@
+export interface XY {
+    x: number;
+    y: number;
+}
+
+export interface XYZ {
+    x: number;
+    y: number;
+    z: number;
+}
+
+export interface XYZW {
+    x: number;
+    y: number;
+    z: number;
+    w: number;
+}
