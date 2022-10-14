c519e00bbc830400e753b16992c4f8d9c3a2fbcc
noname <48761044+noname0310@users.noreply.github.com>
2022-10-14 19:53:01 +0900

    matrix 4x4 wip

--- src/Matrix4x4.ts
+++ src/Matrix4x4.ts
@@ -1,3 +1,7 @@
+import { Matrix4x4Reader, Matrix4x4Writer } from "./Matrix4x4.internalmacro";
+import { Immutable } from "./types/Immutable";
+import { Vector3 } from "./Vector3";
+
 export type Matrix4x4Tuple = [
     number, number, number, number,
     number, number, number, number,
@@ -34,6 +38,46 @@ export class Matrix4x4 {
      */
     public elements: Matrix4x4Tuple;
 
+    /**
+     * Returns whether the matrix is the identity matrix.
+     */
+    public get isIdentity(): boolean {
+        const m = Matrix4x4Reader.$fromTuple!(this.elements);
+        return m.$m11!() === 1 && m.$m22!() === 1 && m.$m33!() === 1 && m.$m44!() === 1  // Check diagonal element first for early out.
+            && m.$m12!() === 0 && m.$m13!() === 0 && m.$m14!() === 0
+            && m.$m21!() === 0 && m.$m23!() === 0 && m.$m24!() === 0
+            && m.$m31!() === 0 && m.$m32!() === 0 && m.$m34!() === 0
+            && m.$m41!() === 0 && m.$m42!() === 0 && m.$m43!() === 0;
+    }
+
+    /**
+     * Gets the translation component of this matrix.
+     * @param out The vector to store the translation component in.
+     * @returns The vector storing the translation component.
+     */
+    public getTranslation(out?: Vector3): Vector3 {
+        out ??= new Vector3();
+
+        const m = Matrix4x4Reader.$fromTuple!(this.elements);
+        return out.set(m.$m41!(), m.$m42!(), m.$m43!());
+    }
+
+    /**
+     * Sets the translation component of this matrix.
+     * @param translation The translation component.
+     * @returns This matrix.
+     */
+    public setTranslation(translation: Immutable<Vector3>): Matrix4x4 {
+        const m = Matrix4x4Writer.$fromTuple!(this.elements);
+        m.$m41!(translation.x);
+        m.$m42!(translation.y);
+        m.$m43!(translation.z);
+        return this;
+    }
+
+    /**
+     * Constructs a Identity 4x4 matrix.
+     */
     public constructor() {
         this.elements = [
             1, 0, 0, 0,
@@ -43,6 +87,9 @@ export class Matrix4x4 {
         ];
     }
 
+    /**
+     * Freezes the matrix 4x4 and returns it.
+     */
     public freeze(): ReadonlyMatrix4x4 {
         Object.freeze(this.elements);
         return Object.freeze(this);
